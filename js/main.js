a7447b55e30d58b84408eda2537adea9cba95cdf
Gerard Davis <gerardguydavis@outlook.com>
2021-07-25 18:20:50 -0400

    Add JS function Update styles and content

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const secret = document.querySelector("#secret");
+const centerSquare = document.querySelector(".center");
+
+secret.addEventListener("mouseover", function() {
+    if (centerSquare.classList.contains("d")) {
+        centerSquare.classList.remove("d");
+        centerSquare.classList.add("secret-image");
+    }
+})
+
+secret.addEventListener("mouseout", function() {
+    if (centerSquare.classList.contains("secret-image")) {
+        centerSquare.classList.remove("secret-image");
+        centerSquare.classList.add("d");
+    }
+})
