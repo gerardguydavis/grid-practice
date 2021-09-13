3382179462a17ae76b95f3e89979dfdf7342c150
Gerard Davis <gerardguydavis@outlook.com>
2021-09-13 13:43:42 -0400

    Added animations for unhover and prevent grid item c from moving on hover

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 const secret = document.querySelector("#secret");
 const centerSquare = document.querySelector(".center");
 
-secret.addEventListener("mouseover", function() {
+secret.addEventListener("mouseover", function () {
     if (centerSquare.classList.contains("d")) {
         centerSquare.classList.remove("d");
         centerSquare.classList.add("secret-image");
     }
 })
 
-secret.addEventListener("mouseout", function() {
+secret.addEventListener("mouseout", function () {
     if (centerSquare.classList.contains("secret-image")) {
         centerSquare.classList.remove("secret-image");
         centerSquare.classList.add("d");
     }
 })
+
+setTimeout(function () {
+    document.body.className = "";
+}, 500);
